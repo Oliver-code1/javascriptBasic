745b508e1214031a9cf554e54fd2ad29285934ea
Oliver-code1 <dubemoliver4@gmail.com>
2024-06-27 22:42:28 +0100

    Javascript Practice

--- hello.js
+++ hello.js
@@ -1,10 +1,57 @@
 var a;
 var b= 2;
-console.log(a)
-a= 4
+console.log(a);
+a= 4;
 
 b=a;
 console.log(b);
+
+var a = 3;
+var b = 2;
+var c = "I am";
+
+a= a + 5;
+b= b + 1;
+c= c + " Oliver_code1";
+
+console.log(c)
+
+
+
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+console.log(titleCaseOver);
+
+var sum= 10 + 10;
+console.log(sum); 
+
+var difference= 30 - 20;
+console.log(difference);
+
+var product= 7 * 4;
+console.log(product);
+
+var quotient= 28 / 7;
+console.log(quotient);
+
+var myVar = 8;
+myVar= myVar + 1;
+console.log(myVar);
+
+var myVarPlus= 8;
+myVarPlus= myVarPlus++; 
+console.log(myVarPlus);
+
+var myNum = 8;
+myNum=myNum++;
+console.log(myNum);
+
 
 
 
