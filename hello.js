81b216a40ecf2ef50c33ea62926d3288f7f6496a
Oliver-code1 <dubemoliver4@gmail.com>
2024-07-02 23:42:41 +0100

    learning js syntax

--- hello.js
+++ hello.js
@@ -1,10 +1,10 @@
 var a;
 var b= 2;
-console.log(a);
+// console.log(a);
 a= 4;
 
 b=a;
-console.log(b);
+// console.log(b);
 
 var a = 3;
 var b = 2;
@@ -14,7 +14,7 @@ a= a + 5;
 b= b + 1;
 c= c + " Oliver_code1";
 
-console.log(c)
+// console.log(c)
 
 
 
@@ -26,58 +26,58 @@ studlyCapVar = 10;
 properCamelCase = "A String";
 titleCaseOver = 9000;
 
-console.log(titleCaseOver);
+// console.log(titleCaseOver);
 
 var sum= 10 + 10;
-console.log(sum); 
+// console.log(sum); 
 
 var difference= 30 - 20;
-console.log(difference);
+// console.log(difference);
 
 var product= 7 * 4;
-console.log(product);
+// console.log(product);
 
 var quotient= 28 / 7;
-console.log(quotient);
+// console.log(quotient);
 
 var myVar = 8;
 myVar= myVar + 1;
-console.log(myVar);
+// console.log(myVar);
 
 var myVarPlus= 8;
 myVarPlus= myVarPlus++;// did not work (it read the above line of code)
-console.log(myVarPlus);
+// console.log(myVarPlus);
 
 var myNum = 8;
 myNum=myNum++;// did not work (it read the above line of code)
-console.log(myNum);
+// console.log(myNum);
 
 var myVarPluss = 8;
 myVarPluss++;
-console.log(myVarPluss);
+// console.log(myVarPluss);
 
 var myVar= 11;
 myVar= myVar -1;
-console.log(myVar);
+// console.log(myVar);
 
 var myVar = 11;
 myVar = myVar--;// did not work (it read the above line of code)
-console.log(myVar);
+// console.log(myVar);
 
 var  number  = 11;
 number--;
-console.log(number);
+// console.log(number);
 
 
 var product= 2.0 * 0*0;
-console.log(product);
+// console.log(product);
 
 var quotient= 4.4 / 2.0;
-console.log(quotient);
+// console.log(quotient);
 
 var remainder;
 remainder = 11 % 3;
-console.log(remainder);
+// console.log(remainder);
 
 
 var a= 3;
@@ -88,9 +88,9 @@ a= a + 12;
 b= 9 + b;
 c= c + 7;
 
-console.log(a);
-console.log(b);
-console.log(c);
+// console.log(a);
+// console.log(b);
+// console.log(c);
 
 var a= 3;
 var b=17;
@@ -100,9 +100,9 @@ a+= 12;
 b+= 9;
 c+= 7;
 
-console.log(a);
-console.log(b);
-console.log(c);
+// console.log(a);
+// console.log(b);
+// console.log(c);
 
 var a = 11;
 var b= 9;
@@ -112,9 +112,9 @@ a=a -6;
 b=b -15;
 c= c-1;
 
-console.log(a);
-console.log(b);
-console.log(c);
+// console.log(a);
+// console.log(b);
+// console.log(c);
 
 var a = 11;
 var b= 9;
@@ -124,9 +124,9 @@ a-=6;
 b-=15;
 c-=1;
 
-console.log(a);
-console.log(b);
-console.log(c);
+// console.log(a);
+// console.log(b);
+// console.log(c);
 
 var a =5;
 var b= 12;
@@ -136,9 +136,9 @@ a =a * 5;
 b= 3 * b;
 c= c * 10;
 
-console.log(a);
-console.log(b);
-console.log(c);
+// console.log(a);
+// console.log(b);
+// console.log(c);
 
 var a= 5;
 var b= 12;
@@ -148,14 +148,97 @@ a*= 5;
 b*= 3;
 c*= 10;
 
-console.log(a);
-console.log(b);
-console.log(c);
+// console.log(a);
+// console.log(b);
+// console.log(c);
 
+var a= 36
+var b= 12
+var c= 33
 
+a= a/12;
+b= b/4;
+c= c/11;
 
+// console.log(a);
+// console.log(b);
+// console.log(c);
 
+a/=12;
+b/=4;
+c/=11;
 
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+var myStr= "I am a \"double quoted\" string inside \"double quotes\"";
+console.log(myStr);
+
+var myStr= "<a href=\"http://www.example.com\" target= \"_blank\">Link</a>";
+console.log(myStr);
+
+var myStr= '<a gref="http://www.example.com" target= "_blank">Link</a>';
+console.log(myStr)
+
+var myStr= `'<a gref="http://www.example.com" target= "_blank"Link</a>'`;/*having both the single quote and double quote and 
+                                                                         this is with the use of backquote or backtick*/
+console.log(myStr)
+
+var myStr= "FirstLine\n \t \\SecondLine\nThirdLine";
+console.log(myStr);
+
+var ourStr= "I come first." + " I come Second."; //same with the next two ones
+console.log(ourStr);
+
+var myStr= "This is the start. " + "This is the end.";
+console.log(myStr);
+
+var ourStr= "I come first. ";
+ourStr+= "I come second."; 
+console.log(ourStr);
+
+var myStr = "This is the first sentence. ";
+myStr+= "This is the second sentence";
+console.log(myStr);
+
+var ourName= "freeCodeCamp";
+var ourString= "Hello, our name is " + ourName + ", how are you?";
+console.log (ourString);
+
+var myName= "Beau";
+var myStr= "My name is " + myName + " and I am well!";
+console.log(myStr)
+
+var anAdjective= "awesome!";
+var ourStr= "freeCodeCamp is ";
+ourStr += anAdjective;
+console.log(ourStr)
+
+var someAdjective= "worthwhile";
+var myStr = "Learning to code is ";
+myStr+=someAdjective;
+console.log(myStr);
+
+var firstNameLength= 0;
+var firstName= "Ada";
+firstNameLength= firstName.length;
+console.log(firstNameLength);
+
+var lastNameLength=0;
+var lastName="Lovelace";
+lastNameLength= lastName.length;
+console.log (lastNameLength);
+
+var firstLetterOfFirstName= "";
+var firstName= "Ada";
+firstLetterOfFirstName= firstName[0]; 
+console.log(firstLetterOfFirstName);
+
+var firstLetterOfLastName= "";
+var lastName= "Chidubem";
+firstLetterOfLastName=lastName[0];
+console.log(firstLetterOfLastName);
 
 
 
